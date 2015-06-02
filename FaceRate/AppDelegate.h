4b299b6414df6af68ed407a208c21101c98f87da
Mac Liu <mlu@iastate.edu>
2015-06-01 21:47:59 -0500

    Initial Commit

---
+++ FaceRate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FaceRate
+//
+//  Created by Mac Liu on 6/1/15.
+//  Copyright (c) 2015 FaceRate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FaceRate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceRate
+//
+//  Created by Mac Liu on 6/1/15.
+//  Copyright (c) 2015 FaceRate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
